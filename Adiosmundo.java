f73398c6c34c650fc244f0f0c9491a81ce946905
Jimena Diaz <diazdelossantosjimena@gmail.com>
2020-10-07 08:36:05 -0500

    Prueba Uno

--- Adiosmundo.java
+++ Adiosmundo.java
@@ -17,6 +17,7 @@ public class Adiosmundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Adios");
     }
     
 }
